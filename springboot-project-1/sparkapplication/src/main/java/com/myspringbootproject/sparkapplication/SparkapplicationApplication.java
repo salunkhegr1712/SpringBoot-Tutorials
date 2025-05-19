bcd225c7a0082fc07c04a6551a005a945f944e4b
salunkhegr1712 <salunkhegr1712@gmail.com>
2025-05-19 20:03:55 +0530

    initial changes

---
+++ springboot-project-1/sparkapplication/src/main/java/com/myspringbootproject/sparkapplication/SparkapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.myspringbootproject.sparkapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkapplicationApplication.class, args);
+	}
+
+}
---
+++ springboot-project-1/sparkapplication/src/test/java/com/myspringbootproject/sparkapplication/SparkapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringbootproject.sparkapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
